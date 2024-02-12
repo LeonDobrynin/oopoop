1acf6075c2ece3c321ad4c6254ffbfceb3f05b37
LeonDobrynin <leon.art.dob@gmail.com>
2024-02-12 17:33:01 +0300

    class rectangle first part

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 
-int main(){
-    std::cout << "123";
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    void setWidth(double w) {
+        width = w;
+    }
+
+    double getWidth() const {
+        return width;
+    }
+
+    void setHeight(double h) {
+        height = h;
+    }
+
+    double getHeight() const {
+        return height;
+    }
+
+    double getArea() const {
+        return width * height;
+    }
+
+    double getPerimeter() const {
+        return 2 * (width + height);
+    }
+};
+
+int main() {
+    Rectangle rect(5.0, 3.0);
+
+    std::cout << "Width: " << rect.getWidth() << std::endl;
+    std::cout << "Height: " << rect.getHeight() << std::endl;
+    std::cout << "Area: " << rect.getArea() << std::endl;
+    std::cout << "Perimeter: " << rect.getPerimeter() << std::endl;
+
+    return 0;
 }
